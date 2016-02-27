1a91192bfbfeb1cb0e9ed679db1cdd686e2dc183
Rukh <gulyagin@mail.com>
2016-02-27 20:37:19 +1000

    Создание репозитория

---
+++ header.php
@@ -0,0 +1,26 @@
+<div id="logo">
+    Logotype
+</div>
+<div id="site_header">
+    <a class="menu_button" href="header.php">
+        О нас
+    </a>
+    <a class="menu_button">
+        Магазин
+        <ul class="hiden_list">
+            <li class="hiden_list_item">Первый пункт меню</li>
+            <li class="hiden_list_item">Второй пункт меню</li>
+            <li class="hiden_list_item">Третий пункт меню</li>
+            <li class="hiden_list_item">Четверты пункт меню</li>
+        </ul>
+    </a>
+    <a class="menu_button">
+        Условия доставки
+    </a>
+    <a class="menu_button">
+        Отзывы
+    </a>
+    <a class="menu_button">
+        Контакты
+    </a>
+</div>
