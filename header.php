2a074275b486fb9b0718fec87be22db5406e0aa0
Rukh <gulyagin@mail.com>
2016-02-28 18:12:42 +1000

    Откат репозитория

--- header.php
+++ header.php
@@ -2,11 +2,11 @@
     Logotype
 </a>
 <div id="site_header">
-    <a class="menu_button" href="index.php?page=1">
-        Главная страница
+    <a class="menu_button" href="index.php?page=info">
+        О нас
     </a>
-    <a class="menu_button" href="index.php?page=2">
-        Сделать заказ
+    <a class="menu_button" href="index.php?page=1">
+        Магазин
         <ul class="hiden_list">
             <li class="hiden_list_item">Первый пункт меню</li>
             <li class="hiden_list_item">Второй пункт меню</li>
@@ -14,7 +14,7 @@
             <li class="hiden_list_item">Четверты пункт меню</li>
         </ul>
     </a>
-    <a class="menu_button">
+    <a class="menu_button" href="index.php?page=2">
         Условия доставки
     </a>
     <a class="menu_button">
