b7ff1b6966d5156d9dfdb176c2ecee99b6de9ce1
Rainos <mazik0496@mail.ru>
2016-03-07 00:38:43 +1000

    добавлена форма

--- order.php
+++ order.php
@@ -3,12 +3,13 @@
 		Составление заказа
 	</div>
 
-<form  name="textarea" method="post" >
-	<textarea  name="comment" cols="100" rows="12"></textarea></p>
-	<p><b>Ваш email</b>
-	<input name="mail" type="email" size="30" required>
-	<b>Ваш телефон</b>
-	<input name="phonenumber" type="tel" size="30"></p>
-	<p><input type="submit" value="Отправить"></p>
+<form  name="textarea" method="post">
+	<textarea  name="comment" cols="120" rows="12"></textarea></p>
+	<p><b>Ваш email:</b>
+	<input name="mail" type="email" size="30" required >
+	<b>Ваш телефон:</b>
+	<input name="phonenumber" type="text" size="30" pattern="[0-9]{11}" required >
+</p>
+	<p><input name="send" type="submit" value="Отправить"></p>
  </form>
 </div>
