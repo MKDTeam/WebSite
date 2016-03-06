998e71ba0c80699100d3eac7ddd757cd0e1ec942
Rainos <mazik0496@mail.ru>
2016-03-07 00:09:49 +1000

    fix

--- order.php
+++ order.php
@@ -5,7 +5,10 @@
 
 <form  name="textarea" method="post" >
 	<textarea  name="comment" cols="100" rows="12"></textarea></p>
-<p><input type="submit" value="Отправить">
-   <input type="reset" value="Очистить"></p>
+	<p><b>Ваш email</b>
+	<input name="mail" type="email" size="30" required>
+	<b>Ваш телефон</b>
+	<input name="phonenumber" type="tel" size="30"></p>
+	<p><input type="submit" value="Отправить"></p>
  </form>
 </div>
