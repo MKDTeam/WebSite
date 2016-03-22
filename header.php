8efb71d03499232a13a59184f7a0bf1dcf3fd2a8
Rukh <gulyagin@mail.com>
2016-03-22 21:27:15 +1000

    Исправлено положение карттинок в меню

--- header.php
+++ header.php
@@ -4,17 +4,21 @@
     <a href="index.php">
         <img id="logo" src="img/logo.svg" style="height: 60px" />
     </a>
+
     <a class="menu_button" href="index.php?page=info">
         Главная
     </a>
+    
     <a class="menu_button" href="index.php?page=order">
-        <img class="graphic_description" src="img/order_description.jpg">
         Сделать заказ 
     </a>
+    <img class="graphic_description" src="img/order_description.jpg">
+    
     <a class="menu_button">
-        <img class="graphic_description" src="img/contacts_description.jpg">
         Контакты
     </a>
+    <img class="graphic_description" src="img/contacts_description.jpg">
+
     <a class="menu_button" href="index.php?page=info">
         О нас
     </a>
