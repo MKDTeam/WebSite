53dc3754e5eeddb868546ddebc965797ddc87529
Rainos <mazik0496@mail.ru>
2016-03-31 20:16:48 +1000

    Закончено добавление текста

--- pages/protection.php
+++ pages/protection.php
@@ -16,15 +16,121 @@
 С 2013 года защиты картера Sheriff окрашиваются по новой технологии, обеспечивающей высочайшую стойкость к коррозии! 
 <p>Детали успешно прошли тест <a class="download" href="http://karter.ru/press/SaltSprayTest.pdf">«840 часов нахождения в камере солевого тумана» </a>(по данным исследовательской лаборатории, США). <a class="download" href="http://karter.ru/press/SaltSprayTest.pdf">Читать подробнее </a>
 	<h2 align="center">Какие зоны должна закрывать защита картера?</h2> 
+
 <figure class="fig">
-	<p>
-	<img  src="img/protection_4.png" width="387" height="282">
-	<img  src="img/protection_5.png" width="387" height="282">
-</p>
-<figcaption>
-защиты картера Sheriff 
-</figcaption>                               
+	<table align="center">
+	
+	<tr><td><img  src="img/protection_4.png" width="387" height="282">
+	<figcaption>
+	защиты картера Sheriff 
+	</figcaption> </td>
+	
+	<td><img  src="img/protection_5.png" width="387" height="282">
+	<figcaption>
+	защиты других производителей 
+	</figcaption>  </td></tr>
+
+	<tr><td><img  src="img/protection_6.png" width="387" height="282">
+	<figcaption>
+	защиты картера Sheriff 
+	</figcaption> </td>
+	
+	<td><img  src="img/protection_7.png" width="387" height="282">
+	<figcaption>
+	защиты других производителей 
+	</figcaption>  </td></tr>
+</table>                             
 </figure>
+<h2 align="center">Какими должны быть зазоры между защитой и агрегатами автомобиля?</h2> 
+<figure class="fig">
+	<table align="center">
+	
+	<tr><td><img  src="img/protection_8.png" width="387" height="282">
+	<figcaption>
+	защиты картера Sheriff 
+	</figcaption> </td>
+	
+	<td><img  src="img/protection_9.png" width="387" height="282">
+	<figcaption>
+	защиты других производителей 
+	</figcaption> </td></tr>
+
+</table>                             
+</figure>
+
+<h2 align="center">Каким должен быть крепеж?</h2> 
+<figure class="fig">
+	<table align="center">
+	
+	<tr><td><img  src="img/protection_10.png" width="387" height="282">
+	<figcaption>
+	защиты картера Sheriff 
+	</figcaption> </td>
+	
+	<td><img  src="img/protection_11.png" width="387" height="282">
+	<figcaption>
+	защиты других производителей 
+	</figcaption>  </td></tr>
+</table>                             
+</figure>
+
+<h2 align="center">Какой должна быть площадь покрытия? 
+</h2> 
+<figure class="fig">
+	<table align="center">
+	
+	<tr><td><img  src="img/protection_12.png" width="387" height="350">
+	<figcaption>
+	защиты картера Sheriff 
+	</figcaption> </td>
+	
+	<td><img  src="img/protection_13.png" width="387" height="350">
+	<figcaption>
+	защиты других производителей 
+	</figcaption>  </td></tr>
+	</table>                             
+	</figure>
+<h2 align="center"> 9 причин установить защиту картера «Sheriff» </h2>
+
+<table >
+<tr><td><img  src="img/protection_save.png" width="50" height="50"></td>
+<td><b>Защита от удара</b><br> 
+Защита картера «Шериф» надежно защитит двигательный отсек Вашего автомобиля, от повреждений при наезде на острые и твердые предметы.</td></tr>
+
+<tr><td><img  src="img/protection_umbrella.png" width="50" height="50"></td>
+<td><b>Защита от удара</b><br> 
+Защита картера «Шериф» надежно защитит двигательный отсек Вашего автомобиля, от повреждений при наезде на острые и твердые предметы.</td></tr>
+
+<tr><td><img  src="img/protection_gradus.png" width="50" height="50"></td>
+<td><b>Защита от перегрева</b><br>  
+Конструкция защиты картера «Шериф» предусматривает наличие отверстий для обдува двигателя, что обеспечивает стабильность температурного режима.</td></tr>
+
+<tr><td><img  src="img/protection_lupa.png" width="50" height="50"></td>
+<td><b>Защита от коррозии</b><br>  
+Порошковое покрытие, наносимое по специальной технологии, имеет высокую устойчивость к повреждению и предотвращает коррозию.</td></tr>
+
+<tr><td><img  src="img/protection_key.png" width="50" height="50"></td>
+<td><b>Защита от шума </b><br> 
+В местах контакта защиты с кузовом предусматриваются резиновые амортизаторы, которые сводят к минимуму шум и вибрацию при движении автомобиля.</td></tr>
+
+<tr><td><img  src="img/protection_dollar.png" width="50" height="50"></td>
+<td><b>Удобство обслуживания</b><br> 
+В защите картера «Шериф» предусмотрены отверстия для слива масла и замены масляного фильтра, что снижает эксплуатационные расходы по обслуживанию автомобиля.</td></tr>
+
+<tr><td><img  src="img/protection_ink.png" width="50" height="50"></td>
+<td><b>Аэродинамика</b><br> 
+Защита картера «Шериф», распределяя потоки воздуха, делает автомобиль более обтекаемым и усиливает сцепление с дорогой.</td></tr>
+
+<tr><td><img  src="img/protection_lock.png" width="50" height="50"></td>
+<td><b>Надежность крепления</b><br>  
+Разработка защит картера «Шериф» ведется с учетом индивидуальных конструктивных особенностей автомобиля, установка осуществляется только на штатные отверстия с использованием качественного крепежа.</td></tr>
+
+<tr><td><img  src="img/protection_health.png" width="50" height="50"></td>
+<td><b>Повышенная прочность</b><br>  
+Защита картера «Шериф» изготавливается методом штамповки, что улучшает её технические характеристики и прочность конструкции.</td></tr>
+
+</table>
+
 </font>
 
 </div>
