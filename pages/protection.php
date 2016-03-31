6da1cca52e4cfd7f2baba863fa392b298142f5a8
Rainos <mazik0496@mail.ru>
2016-03-31 17:18:35 +1000

    Добавление изображения

--- pages/protection.php
+++ pages/protection.php
@@ -7,8 +7,8 @@
 Защита картера разрабатывается с учетом индивидуальных особенностей автомобиля. Её конструкция зависит от типа кузова, объема двигателя, типа КПП, комплектации и прочих параметров. <br>
 Ассортимент Sheriff™ представлен преимущественно штампованными защитами и насчитывает более 2500 моделей из алюминия и стали.
 	<figure class="fig">
-	<img src="img/protection_2.png">
-	<img src="img/protection_3.png">
+	<img  src="img/protection_2.png" width="387" height="282">
+	<img  src="img/protection_3.png" width="387" height="282">
 </figure>
 <a class="download" href="http://karter.ru/catalog2/">Перейти в каталог.</a> Уточняйте применимость для Вашего автомобиля у продавца!
 <h3 align="center">Совершенные характеристики! </h3>
@@ -16,7 +16,15 @@
 С 2013 года защиты картера Sheriff окрашиваются по новой технологии, обеспечивающей высочайшую стойкость к коррозии! 
 <p>Детали успешно прошли тест <a class="download" href="http://karter.ru/press/SaltSprayTest.pdf">«840 часов нахождения в камере солевого тумана» </a>(по данным исследовательской лаборатории, США). <a class="download" href="http://karter.ru/press/SaltSprayTest.pdf">Читать подробнее </a>
 	<h2 align="center">Какие зоны должна закрывать защита картера?</h2> 
-
+<figure class="fig">
+	<p>
+	<img  src="img/protection_4.png" width="387" height="282">
+	<img  src="img/protection_5.png" width="387" height="282">
+</p>
+<figcaption>
+защиты картера Sheriff 
+</figcaption>                               
+</figure>
 </font>
 
 </div>
